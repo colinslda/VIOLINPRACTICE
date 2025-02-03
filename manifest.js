0c0aac7a2cbc207ce1908db53dc080c7928fd232
colinslda <colin.soldati@icloud.com>
2025-02-03 12:18:32 +0100

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,14 +1,19 @@
 {
-  "name": "Suivi de Violon",
-  "short_name": "Violon",
-  "start_url": "index.html",
+  "name": "DA CAPO",
+  "short_name": "DA CAPO",
+  "start_url": "/",
   "display": "standalone",
-  "background_color": "#ffffff",
-  "theme_color": "#000000",
+  "background_color": "#f5f5f5",
+  "theme_color": "#6a1b9a",
   "icons": [
     {
-      "src": "icon.png",
+      "src": "icons/icon-192x192.png",
       "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/icon-512x512.png",
+      "sizes": "512x512",
       "type": "image/png"
     }
   ]
