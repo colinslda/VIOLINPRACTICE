b5bc75f0892231590cd37f71c4ac2375d9fd0b42
colinslda <colin.soldati@icloud.com>
2025-02-03 12:06:57 +0100

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,15 @@
+{
+  "name": "Suivi de Violon",
+  "short_name": "Violon",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#000000",
+  "icons": [
+    {
+      "src": "icon.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    }
+  ]
+}
